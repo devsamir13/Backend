b722dc00862e759eac1388b9e7d377d1c00e8a4d
devsamir13 <samir5827245@gmail.com>
2026-04-23 01:16:33 +0530

    changes

--- index.js
+++ index.js
@@ -4,7 +4,43 @@ const express = require('express')
 
 const app = express()
 
-const port = 4000
+const port = 3000
+
+const githubData = {
+  "login": "devsamir13",
+  "id": 230183303,
+  "node_id": "U_kgDODbhRhw",
+  "avatar_url": "https://avatars.githubusercontent.com/u/230183303?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/devsamir13",
+  "html_url": "https://github.com/devsamir13",
+  "followers_url": "https://api.github.com/users/devsamir13/followers",
+  "following_url": "https://api.github.com/users/devsamir13/following{/other_user}",
+  "gists_url": "https://api.github.com/users/devsamir13/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/devsamir13/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/devsamir13/subscriptions",
+  "organizations_url": "https://api.github.com/users/devsamir13/orgs",
+  "repos_url": "https://api.github.com/users/devsamir13/repos",
+  "events_url": "https://api.github.com/users/devsamir13/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/devsamir13/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": "Samir Ansari",
+  "company": null,
+  "blog": "",
+  "location": "Lucknow, India",
+  "email": null,
+  "hireable": null,
+  "bio": "BCA Final Year Student at BBDU | Aspiring MERN Full Stack Development ",
+  "twitter_username": null,
+  "public_repos": 10,
+  "public_gists": 0,
+  "followers": 4,
+  "following": 6,
+  "created_at": "2025-09-03T14:35:25Z",
+  "updated_at": "2026-01-15T16:56:18Z"
+}
 
 app.get ("/", (req, res) => {
     res.send("Hello World")
@@ -20,6 +56,10 @@ app.get("/login", (req, res) => {
 
 app.get("/youtube", (req, res) => {
     res.send("<h2>Chai aur code</h2>")
+})
+
+app.get("/github", (req, res) => {
+    res.json(githubData)
 })
 
 app.listen(process.env.PORT, () => {
